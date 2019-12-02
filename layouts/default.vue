9e84d21fb253c8e993f1b5afa26395f11d03754a
tanakakz <tanakakz.feb.23@gmail.com>
2019-12-02 16:56:19 +0900

    Unblock commentout categories.

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,10 +16,13 @@
 
       <div class="uk-navbar-right">
         <ul class="uk-navbar-nav">
-          <!-- <li v-for="category in categories">
-                <router-link :to="{ name: 'categories-id', params: { id: category.id }}" tag="a">{{ category.name }}
-                </router-link>
-              </li> -->
+          <li v-for="category in categories">
+            <router-link
+              :to="{ name: 'categories-id', params: { id: category.id } }"
+              tag="a"
+              >{{ category.name }}
+            </router-link>
+          </li>
         </ul>
       </div>
     </nav>
@@ -44,10 +47,14 @@
             <h1 style="font-family: Staatliches;">Nekoka Fun blog</h1>
             <div class="uk-width-1-2@s">
               <ul class="uk-nav-primary uk-nav-parent-icon" uk-nav>
-                <!-- <li v-for="category in categories">
-                            <router-link class="uk-modal-close" :to="{ name: 'categories-id', params: { id: category.id }}" tag="a">{{ category.name }}
-                            </router-link>
-                          </li> -->
+                <li v-for="category in categories">
+                  <router-link
+                    class="uk-modal-close"
+                    :to="{ name: 'categories-id', params: { id: category.id } }"
+                    tag="a"
+                    >{{ category.name }}
+                  </router-link>
+                </li>
               </ul>
             </div>
             <p class="uk-text-light">Built with strapi</p>
