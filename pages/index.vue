0efcafdbc2937371429afdf4422dacc2d43a1016
tanakakz <tanakakz.feb.23@gmail.com>
2019-12-04 23:51:13 +0900

    fix prop typo.

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="uk-section">
       <div class="uk-container uk-container-large">
         <h1>Nekoka Fun Blog</h1>
-        <Articles :atricle="articles"></Articles>
+        <Articles :articles="articles"></Articles>
       </div>
     </div>
   </div>
